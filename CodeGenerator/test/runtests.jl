49bef6a657f381146d90e9bd64490c0e20141670
yeruo <1954402990@qq.com>
2021-07-03 14:30:17 +0800

    test generator

--- CodeGenerator/test/runtests.jl
+++ CodeGenerator/test/runtests.jl
@@ -3,7 +3,17 @@ using CodeGenerator
 
 
 buffer=CodeGenerator.read_grammar()
-g=CodeGenerator.grammar(buffer)
+g=CodeGenerator.Grammar(buffer)
 println(g)
 
 CodeGenerator.prettyshow(stdout,g)
+
+G=CodeGenerator.Generator(g)
+
+CodeGenerator.bind(G,Symbol("<Number>"),()->rand(0:255))
+
+res=CodeGenerator.generate(G,Symbol("Expr");max_depth=2)
+
+for each in res
+      println(each)
+end
