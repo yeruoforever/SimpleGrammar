0fafb1cf76af830eb9e1a8953a32b7d5bdb46917
yeruo <1954402990@qq.com>
2021-07-05 20:46:26 +0800

    new types.

--- CodeGenerator/src/CodeGenerator.jl
+++ CodeGenerator/src/CodeGenerator.jl
@@ -1,14 +1,22 @@
 module CodeGenerator
 import Base:show
 
+# "文法"类型定义
 Terminal = Symbol
 Nonterminal = Symbol
 Token = Union{Terminal,Nonterminal}
 Statement = Vector{Token}
 Production = Dict{Token,Set{Statement}}
 
+# 状态机类型定义
+State = Symbol
+StateSet = Set{State}
+SymbolTabel = Set{Symbol}
+Transition{T} = Dict{State,Dict{Symbol,T}}
 
+include("utils.jl")
 include("grammar.jl")
 include("generator.jl")
+include("NFA.jl")
 
 end # module
